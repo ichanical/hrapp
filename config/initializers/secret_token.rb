5dafeeb0cacc1903681c2a0d0be1341416031fe1
arunkumar339 <arunkumar339@gmail.com>
2012-11-08 01:38:34 +0530

    initial commit for hr app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hrapp::Application.config.secret_token = '4adf39f6307acaf75c363060ea227635c48124bb052ebb061b4a0ceb8e85d2f351234a93ea9a425ea62dec17f03e03cd462c04c1fe30e86b25221999cb099c14'
